ee0981561688115f9c99f5c4837b6d0833746076
Daphne <a.milky.sky@gmail.com>
2018-03-30 16:36:25 -0700

    Fix keywordsSubscript Reducer

--- client/js/reducers/keywordSubscription.js
+++ client/js/reducers/keywordSubscription.js
@@ -9,7 +9,7 @@ const keywordSubscription = (state = defaultState, action) => {
   switch (action.type) {
     case (types.KEYWORD_INPUT_CHANGE):
       return {
-        // ...state,
+        ...state,
         keywordInput: action.data.keywordInput
       }
     default:
